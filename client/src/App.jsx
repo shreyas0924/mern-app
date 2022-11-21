f79872fd19a473d96394c4258629470382294215
shreyas0924 <shreyasp0924@gmail.com>
2022-11-21 22:35:02 +0530

    react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,49 +1,9 @@
-import { useState } from 'react'
+import React from 'react'
 
-import './App.css'
-
-function App() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
-  async function registerUser(event) {
-    event.preventDefault()
-    const response = await fetch('http://localhost:1337/api/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name, email, password }),
-    })
-
-    const data = await response.json()
-    console.log(data)
-  }
+const App = () => {
   return (
-    <div className='App'>
-      <h1>Registration Form: </h1>
-      <form onSubmit={registerUser}>
-        <input
-          type='text'
-          value={name}
-          placeholder='Name'
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type='email'
-          value={email}
-          placeholder='Email'
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type='password'
-          value={password}
-          placeholder='Password'
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <input type='submit' value='Register' />
-      </form>
+    <div>
+      <h1>THis is application</h1>
     </div>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Register />,
+  },
+  {
+    path:'/login',
+    element: <Login />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
