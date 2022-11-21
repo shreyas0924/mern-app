5c139a7c3566d747955f2325b9a01a769286f403
shreyas0924 <shreyasp0924@gmail.com>
2022-11-21 21:38:29 +0530

    data rendering on server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,24 +2,24 @@ import { useState } from 'react'
 
 import './App.css'
 
-async function registerUser(event) {
-  event.preventDefault()
-  const response = await fetch('http://localhost:1337/api/register', {
-    method: 'POST',
-    headers: {
-      'Contect-Type': 'application/json',
-    },
-    body: JSON.stringify({ name, email, password }),
-  })
-
-  const data = await response.json()
-  console.log(data)
-}
-
 function App() {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
+  async function registerUser(event) {
+    event.preventDefault()
+    const response = await fetch('http://localhost:1337/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email, password }),
+    })
+
+    const data = await response.json()
+    console.log(data)
+  }
   return (
     <div className='App'>
       <h1>Registration Form: </h1>
